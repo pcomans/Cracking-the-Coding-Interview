4633579177f6c136f004653fbdd3aadfb23353d6
Philipp Comans <philipp.comans@gmail.com>
2018-04-16 14:19:13 -0700

    Forgot a BST tree property

--- tree.js
+++ tree.js
@@ -4,8 +4,42 @@ class BSTNode {
         this.left = null;
         this.right = null;
     }
+    isBst() {
+        return isBst_impl(this, null, null);
+    }
+}
+
+// Check that min <= x < max
+// Consider null values to indicate no bounds
+function checkValue(x, min, max) {
+    if (min != null && x < min) {
+        return false;
+    }
+    if (max != null && x >= max) {
+        return false;
+    }
+    return true;
+}
+
+// Returns true iff this is the root of a BST.
+function isBst_impl(root, min, max) {
+    if (root.left == null && root.right == null) {
+        // Trivial case
+        return checkValue(root.value, min, max);
+    }
+    if (root.left != null) {
+        if (root.left.value >= root.value || !isBst_impl(root.left, min, root.value)) {
+            return false;
+        }
+    }
+    if (root.right != null) {
+        if (root.right.value <= root.value || !isBst_impl(root.right, root.value, max)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = {
     BSTNode: BSTNode,
-}
+};
--- tree.test.js
+++ tree.test.js
@@ -3,4 +3,22 @@ const Tree = require('./tree');
 test('create simple graph', () => {
     let root = new Tree.BSTNode(5);
     expect(root.value).toBe(5);
+    expect(root.isBst()).toBe(true);
+    root.left = new Tree.BSTNode(8);
+    expect(root.isBst()).toBe(false);
+    root.left = new Tree.BSTNode(3);
+    expect(root.isBst()).toBe(true);
+    root.right = new Tree.BSTNode(8);
+    expect(root.isBst()).toBe(true);
+    root.right.left = new Tree.BSTNode(10);
+    expect(root.isBst()).toBe(false);
+    root.right.left = new Tree.BSTNode(7);
+    root.right.right = new Tree.BSTNode(10);
+    expect(root.isBst()).toBe(true);
+    
+    let root2 = new Tree.BSTNode(20);
+    root2.left = new Tree.BSTNode(10);
+    root2.right = new Tree.BSTNode(30);
+    root2.left.right = new Tree.BSTNode(25);
+    expect(root2.isBst()).toBe(false);
 });
