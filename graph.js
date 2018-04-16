6c1c0574b094bdd4c872684840a01891cac80474
Philipp Comans <philipp.comans@gmail.com>
2018-04-16 13:08:54 -0700

    Some simple graph search stuff in ES6

--- graph.js
+++ graph.js
@@ -5,6 +5,48 @@ class Node {
     }
 }
 
+function dfs(root) {
+    let visited = new Set();
+
+    let dfs_search = function(node) {
+        if (node == null) {
+            return;
+        }
+        visited.add(node);
+        node.children.forEach((c) => {
+            if (!visited.has(c)) {
+                dfs_search(c);
+            }
+        });
+    };
+    dfs_search(root);
+
+    return Array.from(visited);
+}
+
+
+
+function bfs(root) {
+    let visited = new Set();
+    let stack = [];
+
+    stack.push(root);
+
+    while(stack.length != 0) {
+        let current = stack.shift(); // this is probably O(n)
+        visited.add(current);
+
+        current.children.forEach(c => {
+            if (!visited.has(c)) {
+                stack.push(c);
+            }
+        });
+    }
+    return Array.from(visited);
+}
+
 module.exports = {
     Node: Node,
+    bfs: bfs,
+    dfs: dfs,
 };
--- search.test.js
+++ search.test.js
@@ -2,11 +2,28 @@ const Graph = require('./graph');
 
 test('create simple graph', () => {
       let nodes = [];
-      nodes[0] = new Graph.Node("0");
-      expect(nodes[0].name).toBe("0");
-      nodes[1] = new Graph.Node("0");
-      nodes[2] = new Graph.Node("0");
-      nodes[3] = new Graph.Node("0");
-      nodes[4] = new Graph.Node("0");
-      nodes[5] = new Graph.Node("0");
+      times(6, (i) => nodes[i] = new Graph.Node(i.toString()));
+      expect(nodes[0].name).toEqual("0");
+      expect(nodes.length).toBe(6);
+
+      nodes[0].children.push(nodes[1]);
+      nodes[0].children.push(nodes[4]);
+      nodes[0].children.push(nodes[5]);
+
+      nodes[1].children.push(nodes[3]);
+      nodes[1].children.push(nodes[4]);
+
+      nodes[2].children.push(nodes[1]);
+
+      nodes[3].children.push(nodes[2]);
+      nodes[3].children.push(nodes[4]);
+
+      expect(Graph.bfs(nodes[0]).map((n) => n.name)).toEqual(["0", "1", "4", "5", "3", "2",]);
+      expect(Graph.dfs(nodes[0]).map((n) => n.name)).toEqual(["0", "1", "3", "2", "4", "5",]);
 });
+
+function times(n, fun) {
+      for (let i = 0; i < n; i++) {
+            fun(i);
+      }
+}
